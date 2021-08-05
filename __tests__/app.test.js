6b82bdd35586d8845b254e0dfb486dd96eae7b33
Joseph Davis <joethebartenderpdx@gmail.com>
2021-08-05 12:04:54 -0700

    post test created, receiving wild error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,28 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+// import Sauce from '../lib/models/Sauce.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('POST a new sauce', async () => {
+    const res = await request(app)
+      .post('/api/v1/sauces')
+      .send({
+        name: 'super hot',
+        price: '$12.50',
+        heat: 'pretty hot'
+      });
+
+    expect(res.body).toContain({
+      id: 1,
+      name: 'super hot',
+      price: '$12.50',
+      heat: 'pretty hot'
+    });
+  });
+
 });
